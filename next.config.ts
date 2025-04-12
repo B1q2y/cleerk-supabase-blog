160afac16128faa355b823452b5d7490558f130b
B1q2Y <yuya.yoshimoto8888@gmail.com>
2025-04-13 00:07:27 +0900

    Revert "エイリアス修正"
    This reverts commit 150796ab99c3b9f2ee0c1d22689ed028518edb75.

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
-import type { Configuration as WebpackConfig } from "webpack";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack(config: WebpackConfig) {
-    // resolve または alias が未定義の可能性があるため明示的に初期化
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@": path.resolve(__dirname, "src"),
-    };
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
